10e5d5064f1321f488e8083ccac2087652a5d329
Joey Maloney <jmalone4@ycp.edu>
2013-02-12 12:44:36 -0500

    test

--- SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon/Location.java
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon/Location.java
@@ -1,31 +1,32 @@
 package edu.ycp.cs320.fokemon;
 
 public class Location {
-	private int AreaArrayIndex;
-	private int x;
-	private int y;
-public Location(int AreaArrayIndexArea, int x, int y){
-	this.AreaArrayIndex=AreaArrayIndex;
-	this.x=x;
-	this.y=y;
+		private int AreaArrayIndex;
+		private int x;
+		private int y;
+		
+	public Location(int AreaArrayIndex, int x, int y){
+		this.AreaArrayIndex=AreaArrayIndex;
+		this.x=x;
+		this.y=y;
+		}
+	public int GetX(){
+		return x;
 	}
-int GetX(){
-	return x;
-}
-void SetX(int x){
-	this.x=x;
-}
-int GetY(){
-	return y;
-}
-void SetY(int y){
-	this.y=y;
-}
-int GetAreaArrayIndex(){
-	return AreaArrayIndex;
-}
-void SetAreaArrayIndex(int AreaArrayIndex){
-	this.AreaArrayIndex=AreaArrayIndex;
-}
+	public void SetX(int x){
+		this.x=x;
+	}
+	public int GetY(){
+		return y;
+	}
+	public void SetY(int y){
+		this.y=y;
+	}
+	public int GetAreaArrayIndex(){
+		return AreaArrayIndex;
+	}
+	public void SetAreaArrayIndex(int AreaArrayIndex){
+		this.AreaArrayIndex=AreaArrayIndex;
+	}
 
 }
