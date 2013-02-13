dad3df3fc38077479cbddb33f234920474e73a82
Tom Powell <tpowell@ycp.edu>
2013-02-13 15:28:11 -0500

    Pokedex and PokedexEntry classes

---
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon/PokedexEntry.java
@@ -0,0 +1,27 @@
+package edu.ycp.cs320.fokemon;
+
+public class PokedexEntry {
+	private int id;
+	private int spriteFront;
+	private int spriteBack;
+	private String details;
+	private String Name;
+	private int type;
+	private int baseHP;
+	private int baseAtk;
+	private int baseDef;
+	private int baseSpAtk;
+	private int baseSpDef;
+	private int baseSpd;
+	private int incHP;
+	private int incAtk;
+	private int incDef;
+	private int incSpAtk;
+	private int incSpDef;
+	private int incSpd;
+	
+	public PokedexEntry(int _id, int _spriteFront, int _spriteBack, String _details, String _Name, int _type, int _baseHP, int _baseAtk, int _baseDef, 
+			int _baseSpAtk, int _baseSpDef, int _baseSpd,  int _incHP, int _incAtk, int _incDef, int _incSpAtk, int _incSpDef, int _incSpd) {
+		
+	}
+}
