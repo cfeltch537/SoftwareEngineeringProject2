3f53c6bc8c7f9602955b43e02625a11c8ce212ea
Tom Powell <tpowell@ycp.edu>
2013-02-18 09:47:58 -0500

    Added ability to load a csv file into the Pokedex TreeMap, also the main function will print out an instance of this map

--- SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon/PokedexEntry.java
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon/PokedexEntry.java
@@ -1,9 +1,11 @@
 package edu.ycp.cs320.fokemon;
 
+import java.awt.Image;
+
 public class PokedexEntry {
 	private int id;
-	private int spriteFront;
-	private int spriteBack;
+	private Image spriteFront;
+	private Image spriteBack;
 	private String details;
 	private String name;
 	private int type;
@@ -20,13 +22,13 @@ public class PokedexEntry {
 	private int incSpDef;
 	private int incSpd;
 	
-	public PokedexEntry(int _id, int _spriteFront, int _spriteBack, String _details, String _name, int _type, int _baseHP, int _baseAtk, int _baseDef, 
+	public PokedexEntry(int _id, Image _spriteFront, Image _spriteBack, String _name, String _details, int _type, int _baseHP, int _baseAtk, int _baseDef, 
 			int _baseSpAtk, int _baseSpDef, int _baseSpd,  int _incHP, int _incAtk, int _incDef, int _incSpAtk, int _incSpDef, int _incSpd) {
 		this.id=_id;
 		this.spriteFront = _spriteBack;
 		this.spriteBack = _spriteBack;
-		this.details = _details;
 		this.name = _name;
+		this.details = _details;
 		this.type = _type;
 		this.baseHP = _baseHP;
 		this.baseAtk = _baseAtk;
@@ -43,6 +45,29 @@ public class PokedexEntry {
 		
 	}
 
+	public void printEntry() {
+		System.out.printf("\nID = %d",this.id);
+		//System.out.printf("\nSprite Front =  ",this.spriteFront);
+		//System.out.printf(" ",this.spriteBack);
+		System.out.printf("\nName = "+ this.name);
+		System.out.printf("\nDetails = "+ this.details);
+		System.out.printf("\nType = %d ", this.type);
+		System.out.printf("\nHP base = %d ", this.baseHP);
+		System.out.printf("\nAtk base =  %d ", this.baseAtk);
+		System.out.printf("\nDef base = %d ", this.baseDef);
+		System.out.printf("\nSpAtk base = %d ", this.baseSpAtk);
+		System.out.printf("\nSpDef base = %d", this.baseSpDef);
+		System.out.printf("\nSpeed base = %d", this.baseSpd);
+		System.out.printf("\nHP inc = %d ", this.incHP);
+		System.out.printf("\nAtk inc = %d ",this.incAtk);
+		System.out.printf("\nDef inc = %d ", this.incDef);
+		System.out.printf("\nSpAtk inc = %d ", this.incSpAtk);
+		System.out.printf("\nSpDef inc = %d", this.incSpDef);
+		System.out.printf("\nSpeed inc = %d ", this.incSpd);
+		
+	}
+	
+	
 	public int getId() {
 		return id;
 	}
@@ -51,19 +76,19 @@ public class PokedexEntry {
 		this.id = id;
 	}
 
-	public int getSpriteBack() {
+	public Image getSpriteBack() {
 		return spriteBack;
 	}
 
-	public void setSpriteBack(int spriteBack) {
+	public void setSpriteBack(Image spriteBack) {
 		this.spriteBack = spriteBack;
 	}
 
-	public int getSpriteFront() {
+	public Image getSpriteFront() {
 		return spriteFront;
 	}
 
-	public void setSpriteFront(int spriteFront) {
+	public void setSpriteFront(Image spriteFront) {
 		this.spriteFront = spriteFront;
 	}
 
