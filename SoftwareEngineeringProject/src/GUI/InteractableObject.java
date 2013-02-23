3eba7fc966849dc1525be2178178c36d494abe08
Cody Feltch <cfeltch537@gmail.com>
2013-02-23 00:10:07 -0500

    Updated s.t. each Interactable Object contains an array of Interactions

--- SoftwareEngineeringProject/src/GUI/InteractableObject.java
+++ SoftwareEngineeringProject/src/GUI/InteractableObject.java
@@ -1,12 +1,43 @@
 package GUI;
 
+import java.awt.Image;
+import java.util.LinkedList;
+import javax.swing.ImageIcon;
 
-public class InteractableObject {
-	public InteractableObjectName objectName;
-	public Interaction interaction;
+public enum InteractableObject {
+	TallGrass(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass.png"),
+	TallGrassLeftEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_LeftEdge.png"),
+	TallGrassRightEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_RightEdge.png"),
+	TallGrassLowerEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_LowerEdge.png"),
+	TallGrassUpperEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_UpperEdge.png"),
+	TallGrassUpperLeftEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_UpperLeftEdge.png"),
+	TallGrassUpperRightEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_UpperRightEdge.png"),
+	TallGrassLowerLeftEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_LowerLeftEdge.png"),
+	TallGrassLowerRightEdge(new Interaction[] {Interaction.Battle_Wild}, ".\\src/TerrainImages/TallGrass_LowerRightEdge.png"),
+	Trainer(new Interaction[] {Interaction.Battle_Wild}, null);
 	
-	public InteractableObject(InteractableObjectName objectName, Interaction interaction){
-		this.objectName = objectName;
-		this.interaction = interaction;
+	public LinkedList<Interaction> interactionList;
+	public Image img;
+	
+	private InteractableObject(Interaction[] defaultInteraction, String imageSource) {
+		//This constructor add the default interactions and image to the terrain
+		//Default interactions are interactions inherit to the Object
+		//Ex: Battling Wild Pokemon in tall grass, or not being able move onto rocks or buildings
+		for(int i=0; i<defaultInteraction.length;i++){
+			if(defaultInteraction[i]!=null){
+				this.interactionList.add(defaultInteraction[i]);
+			}
+		}
+		//^ As long as the default interaction is not 'null' add to interactionList
+		try{
+			this.img = new ImageIcon(imageSource).getImage();
+		}catch(NullPointerException e){
+			System.out.println("NullPointerException for:" + imageSource + "..." + e);
+		}
+		//^Try to set object image; catch null pointer exceptions
 	}
+	private void addInteraction(Interaction interaction){
+		this.interactionList.add(interaction);
+	}
+	//^Can be used to add additional interactions to an intractable object; if necessary
 }
