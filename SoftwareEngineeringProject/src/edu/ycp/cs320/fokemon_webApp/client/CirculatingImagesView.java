51bead79979906aabafb3fc458da6778cc84f5b0
Cody Feltch <cfeltch537@gmail.com>
2013-03-16 23:19:48 -0400

    Made Views for Map and circulating image canvas(S)

---
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/client/CirculatingImagesView.java
@@ -0,0 +1,132 @@
+
+package edu.ycp.cs320.fokemon_webApp.client;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.canvas.dom.client.CssColor;
+import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.dom.client.Touch;
+import com.google.gwt.event.dom.client.GestureStartEvent;
+import com.google.gwt.event.dom.client.GestureStartHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.event.dom.client.MouseMoveEvent;
+import com.google.gwt.event.dom.client.MouseMoveHandler;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.TouchEndEvent;
+import com.google.gwt.event.dom.client.TouchEndHandler;
+import com.google.gwt.event.dom.client.TouchMoveEvent;
+import com.google.gwt.event.dom.client.TouchMoveHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import edu.ycp.cs320.fokemon_webApp.shared.GUI.Area;
+import edu.ycp.cs320.fokemon_webApp.shared.player.Location;
+import edu.ycp.cs320.fokemon_webApp.shared.player.Player;
+
+public class CirculatingImagesView extends Composite{
+	static final String holderId = "canvasholder";
+	  Canvas canvasTemp;
+	  Canvas backBufferTemp;
+	  LogoGroup logoGroup;
+	  // mouse positions relative to canvas
+	  int mouseX, mouseY;
+
+	  //timer refresh rate, in milliseconds
+	  static final int refreshRate = 100;
+	  
+	  // canvas size, in px
+	  static final int heightTemp = 400;
+	  static final int widthTemp = 400;
+	  
+	  
+	  final CssColor redrawColor = CssColor.make("rgba(255,255,255,0.6)");
+	  Context2d contextTemp;
+	  Context2d backBufferContextTemp;
+		  
+	public CirculatingImagesView(){
+		
+		 canvasTemp = Canvas.createIfSupported();
+		    backBufferTemp = Canvas.createIfSupported();
+		    canvasTemp.setWidth(widthTemp + "px");
+		    canvasTemp.setHeight(heightTemp + "px");
+		    canvasTemp.setCoordinateSpaceWidth(widthTemp);
+		    canvasTemp.setCoordinateSpaceHeight(heightTemp);
+		    backBufferTemp.setCoordinateSpaceWidth(widthTemp);
+		    backBufferTemp.setCoordinateSpaceHeight(heightTemp);
+		    RootPanel.get(holderId).add(canvasTemp);
+		    contextTemp = canvasTemp.getContext2d();
+		    backBufferContextTemp = backBufferTemp.getContext2d();
+		    
+		    // init the objects
+		    logoGroup = new LogoGroup(widthTemp, heightTemp, 18, 165);
+		
+	    initHandlers();
+	    
+	    
+	    // setup timer
+	    final Timer timer = new Timer() {
+	      @Override
+	      public void run() {
+	        doUpdate();
+	      }
+	    };
+	    timer.scheduleRepeating(refreshRate);
+	}
+	  void doUpdate() {
+		   // update the back canvas
+		   backBufferContextTemp.setFillStyle(redrawColor);
+		   backBufferContextTemp.fillRect(0, 0, widthTemp, heightTemp);
+		   logoGroup.update(mouseX, mouseY);
+		   logoGroup.draw(backBufferContextTemp);
+		   // update the front canvas
+		   logoGroup.draw(backBufferContextTemp, contextTemp);
+		    
+	  }
+	  void initHandlers() {
+		    canvasTemp.addMouseMoveHandler(new MouseMoveHandler() {
+		      public void onMouseMove(MouseMoveEvent event) {
+		        mouseX = event.getRelativeX(canvasTemp.getElement());
+		        mouseY = event.getRelativeY(canvasTemp.getElement());
+		      }
+		    });
+
+		    canvasTemp.addMouseOutHandler(new MouseOutHandler() {
+		      public void onMouseOut(MouseOutEvent event) {
+		        mouseX = -200;
+		        mouseY = -200;
+		      }
+		    });
+
+		    canvasTemp.addTouchMoveHandler(new TouchMoveHandler() {
+		      public void onTouchMove(TouchMoveEvent event) {
+		        event.preventDefault();
+		        if (event.getTouches().length() > 0) {
+		          Touch touch = event.getTouches().get(0);
+		          mouseX = touch.getRelativeX(canvasTemp.getElement());
+		          mouseY = touch.getRelativeY(canvasTemp.getElement());
+		        }
+		        event.preventDefault();
+		      }
+		    });
+
+		    canvasTemp.addTouchEndHandler(new TouchEndHandler() {
+		      public void onTouchEnd(TouchEndEvent event) {
+		        event.preventDefault();
+		        mouseX = -200;
+		        mouseY = -200;
+		      }
+		    });
+
+		    canvasTemp.addGestureStartHandler(new GestureStartHandler() {
+		      public void onGestureStart(GestureStartEvent event) {
+		        event.preventDefault();
+		      }
+		    });
+	  }
+}
+	
---
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/client/MapView.java
@@ -0,0 +1,165 @@
+
+package edu.ycp.cs320.fokemon_webApp.client;
+
+import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.dom.client.ImageElement;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import edu.ycp.cs320.fokemon_webApp.shared.GUI.Area;
+import edu.ycp.cs320.fokemon_webApp.shared.player.Location;
+import edu.ycp.cs320.fokemon_webApp.shared.player.Player;
+
+public class MapView extends Composite{
+	static final String holderId = "canvasholder";
+	Canvas backBuffer;
+	Canvas canvas;
+	Context2d context;
+	Context2d backBufferContext;
+	Area[] areaList;
+	Player player;
+	Image img;
+	static int height;
+	static int width;
+	static final int refreshRate = 20;
+	
+	public MapView(){
+		
+		player = new Player(00004, "Cody", true, new Location(0, 20, 20)); // Player Cody
+		areaList = new Area[2];  
+		areaList[0] = new Area();
+		areaList[0].createTallGrassSquare(5, 5, 10, 4);
+		areaList[0].createTallGrassSquare(5, 10, 10, 4);
+		areaList[0].createTallGrassSquare(5, 15, 10, 4);
+		//areaList[0].placeStructure(25, 10, InteractableObject.PokeCenter);
+		areaList[1] = new Area();
+		
+		height = 16*areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[0].length;
+		width = 16*areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain.length;
+		
+		canvas = Canvas.createIfSupported();
+		backBuffer = Canvas.createIfSupported();
+		
+	    // init the canvases
+	    canvas.setWidth(width + "px");
+	    canvas.setHeight(height + "px");
+	    canvas.setCoordinateSpaceWidth(width);
+	    canvas.setCoordinateSpaceHeight(height);
+	    backBuffer.setCoordinateSpaceWidth(width);
+	    backBuffer.setCoordinateSpaceHeight(height);
+	    RootPanel.get(holderId).add(canvas);
+	    context = canvas.getContext2d();
+	    backBufferContext = backBuffer.getContext2d();
+		
+	    initHandlers();
+	    
+	    final Timer timer = new Timer() {
+		      @Override
+		      public void run() {
+		        doUpdate();
+		      }
+		};
+		timer.scheduleRepeating(refreshRate);
+		
+	}
+	
+	void doUpdate() {
+		   // update the back canvas, set to fron canvas
+		   draw(backBufferContext, context);  
+		  }
+	
+	public void draw(Context2d context, Context2d front) {
+		    
+		 context.save();
+		 
+		 //DrawFlooring
+		 for (int height = 0; height < areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain.length; height++) {
+	            for (int width = 0; width < areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height].length; width++) {
+	            	if(areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height][width].flooring.img!=null){
+	            	img = areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height][width].flooring.img;
+	            	context.drawImage((ImageElement) img.getElement().cast(), 16*height, 16*width);
+
+	            	}
+	            }
+	        }
+		 
+		//InteractableObjects
+		for (int height = 0; height < areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain.length; height++) {
+            for (int width = 0; width < areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height].length; width++) {
+            //^Iterate through each terrain in the area
+            	for(int objectIndex = 0; objectIndex<areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height][width].interactableObjectList.size(); objectIndex++){
+	            	if(areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height][width].interactableObjectList.get(objectIndex).img!=null){
+	            		if(areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height][width].interactableObjectList.get(objectIndex).img!=null){
+	            		img = areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height][width].interactableObjectList.get(objectIndex).img;
+		            	context.drawImage((ImageElement) img.getElement().cast(), 16*height, 16*width);
+	            		}
+	            	}	
+            	}	
+            }
+        }
+		//Player
+      for (int height = 0; height < areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain.length; height++) {
+          for (int width = 0; width < areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[height].length; width++) {
+        	  if(height==player.getPlayerLocation().getX()&&width==player.getPlayerLocation().getY()){
+        		img = new Image ("23x25_Trainer_Front.png");
+          		if(areaList[player.getPlayerLocation().getAreaArrayIndex()].terrain[player.getPlayerLocation().getX()][player.getPlayerLocation().getY()].isTallGrassPresent()){
+          			img.setVisibleRect(0, 0, img.getWidth(), img.getHeight()/2);
+          			context.drawImage((ImageElement) img.getElement().cast(), 16*player.getPlayerLocation().getX()-3, 16*player.getPlayerLocation().getY()-15+2);
+          		}else{
+	            	context.drawImage((ImageElement) img.getElement().cast(), 16*player.getPlayerLocation().getX()-3, 16*player.getPlayerLocation().getY()-15+2);
+          		}
+        	  } 	
+          	}
+          }
+		
+		context.restore();
+		front.drawImage(context.getCanvas(), 0, 0);
+	}
+	
+	  void initHandlers() {
+		KeyPressHandler wasdHandler = new KeyPressHandler() {
+			@Override
+			public void onKeyPress(KeyPressEvent event) {
+				int key = event.getUnicodeCharCode();
+				switch(key){
+				case 119: //W; UP
+					player.getPlayerLocation().setY(player.getPlayerLocation().getY()-1);
+					break;
+				case 56: //8; UP
+					player.getPlayerLocation().setY(player.getPlayerLocation().getY()-1);
+					break;
+				case 115: //S; DOWN
+					player.getPlayerLocation().setY(player.getPlayerLocation().getY()+1);
+					break;
+				case 50: //2; DOWN
+					player.getPlayerLocation().setY(player.getPlayerLocation().getY()+1);
+					break;
+				case 97: //A, LEFT
+					player.getPlayerLocation().setX(player.getPlayerLocation().getX()-1);
+					break;
+				case 52: //4; LEFT
+					player.getPlayerLocation().setX(player.getPlayerLocation().getX()-1);
+					break;
+				case 100: //D; RIGHT 
+					player.getPlayerLocation().setX(player.getPlayerLocation().getX()+1);
+					break;
+				case 54: //6; RIGHT 
+					player.getPlayerLocation().setX(player.getPlayerLocation().getX()+1);
+					break;
+				}
+				//System.out.println(key); //For Debug
+			}
+		};
+		canvas.addDomHandler(wasdHandler, KeyPressEvent.getType());
+		canvas.setFocus(true);
+	 }
+	
+}
+
+
+
