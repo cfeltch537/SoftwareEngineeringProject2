fd56189880f7ba2847df0eed9ff41f463704de4c
Derrike Hill <dhill3@ycp.edu>
2013-04-12 09:42:42 -0400

    finished the move database

--- SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/shared/MoveClasses/MoveDataBase.java
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/shared/MoveClasses/MoveDataBase.java
@@ -1214,82 +1214,373 @@ public class MoveDataBase {
     	move.setEffect(effectList);
 		break;
 	case Sand_Attack:
+    	move.setDamage(0);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.GROUND);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Scratch:
+    	move.setDamage(40);
+    	move.setAccuracy(100);
+    	move.setMaxPP(35);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Screech:
+    	move.setDamage(0);
+    	move.setAccuracy(85);
+    	move.setMaxPP(40);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Seismic_Toss:
+    	move.setDamage(0);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.FIGHTING);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Selfdestruct:
+    	move.setDamage(200);
+    	move.setAccuracy(100);
+    	move.setMaxPP(5);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Sharpen:
+    	move.setDamage(0);
+    	move.setAccuracy(-1);
+    	move.setMaxPP(30);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Sing:
+    	move.setDamage(0);
+    	move.setAccuracy(55);
+    	move.setMaxPP(15);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effect.setEffectIndex(EffectType.SLEEP);
+    	effect.setEffectChance(100);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Skull_Bash:
+    	move.setDamage(100);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Sky_Attack:
+    	move.setDamage(140);
+    	move.setAccuracy(90);
+    	move.setMaxPP(5);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.FLYING);
+    	
+//    	effect.setEffectIndex(EffectType.FLINCH);
+//    	effect.setEffectChance(30);
+//    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Slam:
+    	move.setDamage(80);
+    	move.setAccuracy(75);
+    	move.setMaxPP(20);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Slash:
+    	move.setDamage(70);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Sleep_Powder:
+    	move.setDamage(0);
+    	move.setAccuracy(75);
+    	move.setMaxPP(15);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.GRASS);
+    	
+    	effect.setEffectIndex(EffectType.SLEEP);
+    	effect.setEffectChance(100);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Sludge:
+    	move.setDamage(65);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.POISON);
+    	
+    	effect.setEffectIndex(EffectType.POISON);
+    	effect.setEffectChance(30);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Smog:
+    	move.setDamage(20);
+    	move.setAccuracy(70);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.POISON);
+    	
+    	effect.setEffectIndex(EffectType.POISON);
+    	effect.setEffectChance(40);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case SmokeScreen:
+    	move.setDamage(0);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Softboiled:
+    	move.setDamage(0);
+    	move.setAccuracy(-1);
+    	move.setMaxPP(10);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case SolarBeam:
+    	move.setDamage(120);
+    	move.setAccuracy(100);
+    	move.setMaxPP(10);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.GRASS);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case SonicBoom:
+    	move.setDamage(20);
+    	move.setAccuracy(90);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Spike_Cannon:
+    	move.setDamage(20);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Splash:
+    	move.setDamage(0);
+    	move.setAccuracy(-1);
+    	move.setMaxPP(40);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Spore:
-     	move.setMaxPP(10);
-     	move.setPhysical(false);
-     	
-     	effect.setEffectIndex(EffectType.SLEEP);
-     	effect.setEffectChance(100);
-     	effect.setMagnitude(0);
-     	effectList.add(effect);
-     	
-     	move.setEffect(effectList);
+    	move.setDamage(0);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.GRASS);
+    	
+    	effect.setEffectIndex(EffectType.SLEEP);
+    	effect.setEffectChance(100);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
      	break;
 	case Stomp:
+    	move.setDamage(65);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+//    	effect.setEffectIndex(EffectType.FLINCH);
+//    	effect.setEffectChance(30);
+//    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Strength:
+    	move.setDamage(80);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case String_Shot:
+    	move.setDamage(0);
+    	move.setAccuracy(95);
+    	move.setMaxPP(40);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.BUG);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Struggle:
+    	move.setDamage(50);
+    	move.setAccuracy(100);
+    	move.setMaxPP(0);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Stun_Spore:
+    	move.setDamage(0);
+    	move.setAccuracy(75);
+    	move.setMaxPP(30);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.GRASS);
+    	
+    	effect.setEffectIndex(EffectType.PARALYZE);
+    	effect.setEffectChance(100);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Submission:
+    	move.setDamage(80);
+    	move.setAccuracy(80);
+    	move.setMaxPP(25);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.FIGHTING);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Substitute:
+//    	move.setDamage(0);
+//    	move.setAccuracy(-1);
+//    	move.setMaxPP(10);
+//    	move.setPhysical(false);
+//    	move.setPokeType(PokeType.NORMAL);
+//    	
+//    	effectList.add(0, effect);
+//    	move.setEffect(effectList);
 		break;
 	case Super_Fang:
+//    	move.setDamage(0);
+//    	move.setAccuracy(90);
+//    	move.setMaxPP(10);
+//    	move.setPhysical(true);
+//    	move.setPokeType(PokeType.NORMAL);
+//    	
+//    	effectList.add(0, effect);
+//    	move.setEffect(effectList);
 		break;
 	case Supersonic:
+    	move.setDamage(0);
+    	move.setAccuracy(55);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effect.setEffectIndex(EffectType.CONFUSION);
+    	effect.setEffectChance(100);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Surf:
+    	move.setDamage(95);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.WATER);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Swift:
+    	move.setDamage(60);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Swords_Dance:
+    	move.setDamage(0);
+    	move.setAccuracy(-1);
+    	move.setMaxPP(30);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Tackle:
-		move.setDamage(50);
+    	move.setDamage(50);
     	move.setAccuracy(100);
     	move.setMaxPP(35);
     	move.setPhysical(true);
@@ -1299,55 +1590,253 @@ public class MoveDataBase {
     	move.setEffect(effectList);
 		break;
 	case Tail_Whip:
+    	move.setDamage(0);
+    	move.setAccuracy(100);
+    	move.setMaxPP(30);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Take_Down:
+    	move.setDamage(90);
+    	move.setAccuracy(85);
+    	move.setMaxPP(20);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Teleport:
+//    	move.setDamage(0);
+//    	move.setAccuracy(-1);
+//    	move.setMaxPP(20);
+//    	move.setPhysical(false);
+//    	move.setPokeType(PokeType.PSYCHIC);
+//    	
+//    	effectList.add(0, effect);
+//    	move.setEffect(effectList);
 		break;
 	case Thrash:
+    	move.setDamage(120);
+    	move.setAccuracy(100);
+    	move.setMaxPP(10);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Thunder:
+    	move.setDamage(120);
+    	move.setAccuracy(70);
+    	move.setMaxPP(10);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.ELECTRIC);
+    	
+    	effect.setEffectIndex(EffectType.PARALYZE);
+    	effect.setEffectChance(30);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case ThunderPunch:
+    	move.setDamage(75);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.ELECTRIC);
+    	
+    	effect.setEffectIndex(EffectType.PARALYZE);
+    	effect.setEffectChance(10);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case ThunderShock:
+    	move.setDamage(40);
+    	move.setAccuracy(100);
+    	move.setMaxPP(30);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.ELECTRIC);
+    	
+    	effect.setEffectIndex(EffectType.PARALYZE);
+    	effect.setEffectChance(10);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
     case Thunder_Wave:
-    	move.setMaxPP(10);
+    	move.setDamage(0);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
     	move.setPhysical(false);
     	move.setPokeType(PokeType.ELECTRIC);
     	
     	effect.setEffectIndex(EffectType.PARALYZE);
     	effect.setEffectChance(100);
     	effect.setMagnitude(0);
-    	effectList.add(effect);
-    		break;
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
+    	break;
 	case Thunderbolt:
+    	move.setDamage(95);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.ELECTRIC);
+    	
+    	effect.setEffectIndex(EffectType.PARALYZE);
+    	effect.setEffectChance(10);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Toxic:
+    	move.setDamage(0);
+    	move.setAccuracy(90);
+    	move.setMaxPP(10);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.POISON);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Transform:
+//    	move.setDamage(0);
+//    	move.setAccuracy(-1);
+//    	move.setMaxPP(10);
+//    	move.setPhysical(false);
+//    	move.setPokeType(PokeType.NORMAL);
+//    	
+//    	effectList.add(0, effect);
+//    	move.setEffect(effectList);
 		break;
 	case Tri_Attack:
+    	move.setDamage(80);
+    	move.setAccuracy(100);
+    	move.setMaxPP(10);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effect.setEffectIndex(EffectType.PARALYZE);
+    	effect.setEffectChance(7);
+    	effect.setMagnitude(0);
+    	effect.setEffectIndex(EffectType.BURN);
+    	effect.setEffectChance(7);
+    	effect.setMagnitude(0);
+    	effect.setEffectIndex(EffectType.FREEZE);
+    	effect.setEffectChance(7);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Twineedle:
+    	move.setDamage(25);
+    	move.setAccuracy(100);
+    	move.setMaxPP(20);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.BUG);
+    	
+    	effect.setEffectIndex(EffectType.POISON);
+    	effect.setEffectChance(20);
+    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case ViceGrip:
+    	move.setDamage(55);
+    	move.setAccuracy(100);
+    	move.setMaxPP(30);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Vine_Whip:
+    	move.setDamage(35);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.GRASS);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Water_Gun:
+    	move.setDamage(40);
+    	move.setAccuracy(100);
+    	move.setMaxPP(25);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.WATER);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Waterfall:
+    	move.setDamage(80);
+    	move.setAccuracy(100);
+    	move.setMaxPP(15);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.WATER);
+    	
+//    	effect.setEffectIndex(EffectType.FLINCH);
+//    	effect.setEffectChance(20);
+//    	effect.setMagnitude(0);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Whirlwind:
+//    	move.setDamage(0);
+//    	move.setAccuracy(100);
+//    	move.setMaxPP(20);
+//    	move.setPhysical(false);
+//    	move.setPokeType(PokeType.NORMAL);
+//    	
+//    	effectList.add(0, effect);
+//    	move.setEffect(effectList);
 		break;
 	case Wing_Attack:
+    	move.setDamage(60);
+    	move.setAccuracy(100);
+    	move.setMaxPP(35);
+    	move.setPhysical(true);
+    	move.setPokeType(PokeType.FLYING);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Withdraw:
+    	move.setDamage(0);
+    	move.setAccuracy(-1);
+    	move.setMaxPP(40);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.WATER);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
 		break;
 	case Wrap:
-		break;
+    	move.setDamage(15);
+    	move.setAccuracy(90);
+    	move.setMaxPP(20);
+    	move.setPhysical(false);
+    	move.setPokeType(PokeType.NORMAL);
+    	
+    	effectList.add(0, effect);
+    	move.setEffect(effectList);
+    	break;
 	default:
 		break;
 	}
