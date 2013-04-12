b0ccac5a100bd633a65ad59f13abbf6394a778a9
Cody <cfeltch@ycp.edu>
2013-04-11 20:18:38 -0400

    Good Commit. Joey is falling off arene

--- SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/client/BattleView.java
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/client/BattleView.java
@@ -384,7 +384,11 @@ public class BattleView extends Composite{
 
 		 // Opponent Battling Pokemon
 		
-		opponentPokemon = new Image("PokemonSprites/" + battle.getOpponent().getTeam(battle.getOpponent().getCurrentPokemonIndex()).getInfo().getPokeName() + ".png");//This should set to a pokemons ID specific Image
+		//opponentPokemon = new Image("PokemonSprites/" + battle.getOpponent().getTeam(battle.getOpponent().getCurrentPokemonIndex()).getInfo().getPokeName() + ".png");//This should set to a pokemons ID specific Image
+		if(opponentPokemon!=null){
+			battlePanel.remove(opponentPokemon);
+		}
+		opponentPokemon = new Image("PokemonSprites/Snorlax.png");//This should set to a pokemons ID specific Image
 		opponentPokemon.setVisible(false);
 		battlePanel.add(opponentPokemon);
 		battlePanel.getElement().getStyle().setPosition(Position.RELATIVE);
