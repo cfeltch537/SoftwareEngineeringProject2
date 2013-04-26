7a1dc4745f027928b6f84c93b4251c9029cb9acd
Joey Maloney <jmalone4@ycp.edu>
2013-04-26 09:13:59 -0400

    working off of game

--- SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/shared/PokemonClasses/Pokemon.java
+++ SoftwareEngineeringProject/src/edu/ycp/cs320/fokemon_webApp/shared/PokemonClasses/Pokemon.java
@@ -32,8 +32,7 @@ public class Pokemon {
 			gender = false;
 		else
 			gender = true;
-		PokeInfo information = new PokeInfo(entry.getPokeID(), Game
-				.getUser().getPlayerID(), entry.getPokeName(),
+		PokeInfo information = new PokeInfo(entry.getPokeID(), Game.getUser().getPlayerID(), entry.getPokeName(),
 				entry.getPokeName(), gender, entry.getType(), lvl, 0,255);
 		Move move1 = MoveDataBase.generateMove(MoveName.Tackle);
 		ArrayList<Move> moves = new ArrayList<Move>();
