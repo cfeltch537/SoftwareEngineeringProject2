c10983dc0eb864a367e4bb4052829e785fd6d7a3
Joey Maloney <jmalone4@ycp.edu>
2013-05-09 15:34:27 -0400

    test

---
+++ SoftwareEngineeringProject/junit/edu/ycp/cs320/itemTests/TestAllItems.java
@@ -0,0 +1,16 @@
+package edu.ycp.cs320.itemTests;
+
+import org.junit.Test;
+
+import edu.ycp.cs320.fokemon_webApp.shared.ItemClasses.ItemDatabase;
+import edu.ycp.cs320.fokemon_webApp.shared.ItemClasses.ItemName;
+
+public class TestAllItems {
+	
+	@Test
+	public void setUp()	{
+		for(int i = 0; i < ItemName.getLength(); i++)	{
+			ItemDatabase.generateItem(ItemName.getItem(i), 0);
+		}
+	}
+}
---
+++ SoftwareEngineeringProject/junit/edu/ycp/cs320/moveTests/TestAllMoves.java
@@ -0,0 +1,16 @@
+package edu.ycp.cs320.moveTests;
+
+import org.junit.Test;
+
+import edu.ycp.cs320.fokemon_webApp.shared.MoveClasses.MoveDataBase;
+import edu.ycp.cs320.fokemon_webApp.shared.MoveClasses.MoveName;
+
+public class TestAllMoves {
+	
+	@Test
+	public void setUp()	{
+		for(int i = 0; i < MoveName.getLength(); i++)	{
+			MoveDataBase.generateMove(MoveName.getMove(i));
+		}
+	}
+}
